0b66e7b6e1c0c646d6314afa2346ab41881b0cd6
john <grafpoo@gmail.com>
2021-06-02 11:23:52 -0400

    set up CA infra

--- cluster1/scripts/setup-certs.sh
+++ cluster1/scripts/setup-certs.sh
@@ -2,4 +2,5 @@
  apt install -y python-pip
  pip install pexpect
 
+mkdir -p /vagrant/certs
  python3 /vagrant/scripts/create-certs.py
--- create-certs.py
+++ create-certs.py
@@ -38,7 +38,43 @@ domain = sys.argv[1]
 ca_keyfile_name = f"/vagrant/certs/{domain}CA.key"
 ca_pemfile_name = f"/vagrant/certs/{domain}CA.pem"
 
-ca_password = sys.argv.count > 1 ? sys.argv[1] : "password"
+ca_password = sys.argv[1] if len(sys.argv) > 1 else "password"
+
+# create certificate authority
+
+# generate CA keyfile
+
+ca_key_command = f"openssl genrsa -des3 -out {ca_keyfile_name} 2048"
+subprocess.call(["echo", ca_key_command])
+child = pexpect.spawn(ca_key_command)
+
+child.expect('.*', timeout=5)
+child.sendline(ca_password)
+child.expect('.*', timeout=5)
+child.sendline(ca_password)
+
+ca_pem_command = f"openssl req -x509 -new -nodes -key {ca_keyfile_name} -sha256 -days 1825 -out {ca_pemfile_name}"
+subprocess.call(["echo", ca_pem_command])
+child = pexpect.spawn(ca_pem_command)
+
+child.expect('.*', timeout=5)
+child.sendline(ca_password)
+child.expect('Country.*\]:', timeout=5)
+child.sendline('US')
+child.expect('State.*\]:')
+child.sendline('')
+child.expect('Locality.*\]:')
+child.sendline('')
+child.expect('Organization.*\]:')
+child.sendline('')
+child.expect('Organizational.*\]:')
+child.sendline('')
+child.expect('Common.*\]:')
+child.sendline(domain)
+child.expect('Email.*\]:')
+child.sendline('')
+
+# now create certs for machines, signed by CA
 
 for host in ["harbor", "gitlab"]:
     keyfile_name = f"/vagrant/certs/{domain}_{host}.key"
@@ -90,5 +126,10 @@ for host in ["harbor", "gitlab"]:
 
     # generate certificate
 
-    os.system(f"openssl x509 -req -in {csrfile_name} -CA /vagrant/ca/k8sCA.pem -CAkey /vagrant/ca/k8sCA.key -CAcreateserial -out {crtfile_name} -days 825 -sha256 -extfile {extfile_name}")
+    crt_command = f"openssl x509 -req -in {csrfile_name} -CA {ca_pemfile_name} -CAkey {ca_keyfile_name} -CAcreateserial -out {crtfile_name} -days 825 -sha256 -extfile {extfile_name}"
+    subprocess.call(["echo", crt_command])
+    child = pexpect.spawn(crt_command)
+
+    child.expect('Enter.*', timeout=5)
+    child.sendline(ca_password)
 
